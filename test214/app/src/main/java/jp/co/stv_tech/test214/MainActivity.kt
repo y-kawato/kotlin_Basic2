1e07231e8c6985bea12c513d0f60c617467da320
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-05-21 09:20:19 +0900

    2.1.4

---
+++ test214/app/src/main/java/jp/co/stv_tech/test214/MainActivity.kt
@@ -0,0 +1,40 @@
+package jp.co.stv_tech.test214
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var container: LinearLayout
+    private lateinit var inputMethodManager: InputMethodManager
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("MainActivity", "onCreate")
+
+        val editText = findViewById<EditText>(R.id.editText)
+
+        container = findViewById<LinearLayout>(R.id.container)
+
+        inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        Log.i("MainActivity", "onTouchEvent")
+
+        inputMethodManager.hideSoftInputFromWindow(container.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
+
+
+        container.requestFocus()
+
+        return false
+    }
+}
