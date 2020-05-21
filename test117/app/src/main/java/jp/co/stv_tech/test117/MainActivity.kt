c3e261d84bd2f02a3e605a9c29f3455b91ab19f0
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-05-21 09:16:36 +0900

    1.1.7

---
+++ test117/app/src/main/java/jp/co/stv_tech/test117/MainActivity.kt
@@ -0,0 +1,39 @@
+package jp.co.stv_tech.test117
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val result : Result = Math()
+        result.calculate()
+
+    }
+
+    public interface Result {
+
+
+        fun calculate()
+
+
+    }
+    class Math : Result {
+
+        override fun calculate() {
+            val a = 2
+            val b = 3
+            println(a + b)
+
+        }
+    }
+    //fun main() {
+
+      //  val result = println(a+b)
+        //result.result()
+    //}
+
+}
