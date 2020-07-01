ca48901744a9c033b9de0d0e77f0e7deb257be53
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-07-01 09:13:15 +0900

    2.1.14修正

--- test2114/app/src/main/java/jp/co/stv_tech/test2114/MainActivity.kt
+++ test2114/app/src/main/java/jp/co/stv_tech/test2114/MainActivity.kt
@@ -1,10 +1,8 @@
 package jp.co.stv_tech.test2114
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity() : AppCompatActivity() {
@@ -13,7 +11,7 @@ class MainActivity() : AppCompatActivity() {
 
         private const val RESULT_SUBACTIVITY = 1000
     }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
